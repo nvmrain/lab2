1efeab77759ca4e3b7795abff8e76c3ce94c7e8a
Xiaoyu <yxy123yu@gmail.com>
2018-01-18 18:01:06 -0800

    fix bug displaying twice

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,6 +1,5 @@
 <div class="row">
     {authors}
-    <div class="span4"><img src="/pix/{who}.jpg" title="{who} plays {role}"/></div>
     <a href="/welcome/show/{key}">
         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
     </a>
