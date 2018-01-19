1b6a027edff8005c5a17c01a46ae30bba6cf039d
Xiaoyu <yxy123yu@gmail.com>
2018-01-18 17:54:22 -0800

    add & modify quotes display

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,5 +1,7 @@
 <div class="row">
     {authors}
-    <div class="span4"><img src="/pix/{who}.jpg"/></div>
+    <a href="/welcome/show/{key}">
+        <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+    </a>
     {/authors}
 </div>
